5b3bffd6168885306cc509469bf3bdcb9a3a324b
nusspez <nusspez@gmail.com>
2020-07-09 14:03:41 -0500

    version 1.0

--- flsk_app/frontend/src/components/GpioOnOff.vue
+++ flsk_app/frontend/src/components/GpioOnOff.vue
@@ -66,7 +66,7 @@ export default {
   },
   mounted () {
     axios
-      .get('http://192.168.1.71:5000/api/v1.0/actions/')
+      .get('http://127.0.0.1:5000/api/v1.0/actions/')
       .then(response => (this.mensaje = response))
   }
 }
