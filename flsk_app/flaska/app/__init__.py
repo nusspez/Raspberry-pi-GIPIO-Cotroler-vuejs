cd23ee736c3a64396e88f916d730b84a51a0f6d9
nusspez <nusspez@gmail.com>
2020-07-09 21:56:16 -0500

    version 1.0

--- flsk_app/flaska/app/__init__.py
+++ flsk_app/flaska/app/__init__.py
@@ -3,3 +3,6 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+for i in range(2,28):
+    actions[str(i)] = {'state':'0'}
--- flsk_app/flaska/index.py
+++ flsk_app/flaska/index.py
@@ -3,11 +3,9 @@ from app.resources.control import Pin
 from app.views.AppViews import HelloMesage
 actions = {}
 
-for i in range(2,28):
-    actions[str(i)] = {'state':'0'}
 
 app.add_url_rule('/api/v1.0/mensaje', view_func=HelloMesage.as_view('Hello'))
 
-
+print(actions)
 if __name__ == '__main__':
     app.run(debug=False, port=5000, host='0.0.0.0')
